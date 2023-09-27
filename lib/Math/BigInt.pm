573ab85496e60e2c0c425cc23ba9c331fa29f214
Peter John Acklam <pjacklam@gmail.com>
2023-09-27 21:59:49 +0200

    Increment version number

--- lib/Math/BigInt.pm
+++ lib/Math/BigInt.pm
@@ -23,7 +23,7 @@ use warnings;
 use Carp          qw< carp croak >;
 use Scalar::Util  qw< blessed refaddr >;
 
-our $VERSION = '1.999841';
+our $VERSION = '1.999842';
 $VERSION =~ tr/_//d;
 
 require Exporter;
